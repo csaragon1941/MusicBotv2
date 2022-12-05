15a81d6daccc5f0ad080385ead765e8caf430a15
Christina Aragon <62121469+csaragon1941@users.noreply.github.com>
2022-12-05 13:04:43 -0500

    Update main.js

--- MusicBotv2/main.js
+++ MusicBotv2/main.js
@@ -1,10 +1,10 @@
 const { Player } = require('discord-player');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js'); //Important to declare because in Discord v14 needs it to connect into the Discord Api
 
 global.client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds,   //Guild = server
+        GatewayIntentBits.GuildMembers, //These guild declarations allow permissions for the bot essentially 
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.MessageContent
@@ -12,11 +12,11 @@ global.client = new Client({
    disableMentions: 'everyone',
 });
 
-client.config = require('./config');
+client.config = require('./config'); //the configuration hold important info for the bot to work such as the TOKEN
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
-require('./src/loader');
+require('./src/loader'); //Under Contruction 
 require('./src/events');
 
-client.login(client.config.app.token);
+client.login(client.config.app.token); //Logs the bot into the server
