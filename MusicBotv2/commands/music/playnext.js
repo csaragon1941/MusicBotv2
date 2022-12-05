5760b4ac285446569fa43e124649eaacc36a2ce5
Christina Aragon <62121469+csaragon1941@users.noreply.github.com>
2022-12-05 13:13:36 -0500

    Update playnext.js

--- MusicBotv2/commands/music/playnext.js
+++ MusicBotv2/commands/music/playnext.js
@@ -1,14 +1,17 @@
+//Playnext command
+//When user enters '/playnext' it will play the next song you want within Discord.
+
 const { ApplicationCommandOptionType } = require('discord.js');
 const { QueryType } = require('discord-player');
 
 module.exports = {
-    name: 'playnext',
-    description: "song you want to playnext",
+    name: 'playnext', //command
+    description: "song you want to playnext", //what the command does
     voiceChannel: true,
     options: [
         {
             name: 'song',
-            description: 'the song you want to playnext',
+            description: 'the song you want to playnext', 
             type: ApplicationCommandOptionType.String,
             required: true,
         }
@@ -17,22 +20,25 @@ module.exports = {
     async execute({ inter }) {
 	await inter.deferReply();
         const queue = player.getQueue(inter.guildId);
-
+	
+	    //if there is not music playing
         if (!queue || !queue.playing) return inter.editReply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
 
         const song = inter.options.getString('song');
 
         const res = await player.search(song, {
-            requestedBy: inter.member,
+            requestedBy: inter.member, //when it searches for the song
             searchEngine: QueryType.AUTO
         });
-
+	
+	    //when it cant find the track
         if (!res || !res.tracks.length) return inter.editReply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
 
        if (res.playlist) return inter.editReply({ content: `This command dose not support playlist's ${inter.member}... try again ? ❌`, ephemeral: true });
 
         queue.insert(res.tracks[0], 0)
-
+	
+	    //when it plays the track
         await inter.editReply({ content:`Track has been inserted into the queue... it will play next 🎧`});
 
     }
